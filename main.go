8d123d89ae1df178262144e9e29cc131c1774013
Genesio Lini <lini.genesio@gmail.com>
2016-09-20 14:54:39 +0200

    commit

--- main.go
+++ main.go
@@ -14,26 +14,49 @@ import (
 
 var wg sync.WaitGroup
 
+type Artifact struct {
+	Path       string `json:"path"`
+	PrettyPath string `json:"pretty_path"`
+	NodeIndex  string `json:"nodex_index"`
+	Url        string `json:"url"`
+}
+
+
 func main() {
-	vcs := flag.String("vcs", "bitbucket", "CircleCI VCS Type")
-	user := flag.String("user", "systemlogic", "CircleCI Organization Name")
-	project := flag.String("project", "produseal-www", "CircleCI Organization Name")
+	vcs := flag.String("vcs", "", "CircleCI VCS Type")
+	user := flag.String("user", "", "CircleCI Organization Name")
+	project := flag.String("project", "", "CircleCI Organization Name")
 	build := flag.String("build", "latest", "Project Build Number (or 'latest')")
+	files := flag.Uint("files", 4, "Maximum number of concurrent downloads")
+	downloadFolder := flag.String("target", ".", "Target download folder")
 	flag.Parse()
+
+	if (*vcs == "" || *user == "" || *project == "" || *build == "") {
+		flag.Usage()
+		os.Exit(1)
+	}
 
 	circleToken := os.Getenv("CIRCLE_CI_TOKEN")
 
-	filesToDownload := make(chan ArtifactJson, 4)
-	go downloadArtifacts(circleToken, filesToDownload)
+	if (circleToken == "") {
+		fmt.Print("CIRCLE_CI_TOKEN environment variable must be set")
+		os.Exit(2)
+	}
+
+	filesToDownload := make(chan Artifact, int(*files))
+	go downloadArtifacts(circleToken, *downloadFolder, filesToDownload)
 
 	apiClient := sling.New().Base("https://circleci.com/api/v1.1/")
 	apiClient.Set("Accept", "application/json")
 
-	result := []ArtifactJson{}
+	result := []Artifact{}
 	q := fmt.Sprintf("project/%s/%s/%s/%s/artifacts?circle-token=%s", *vcs, *user, *project, *build, circleToken)
 	s := apiClient.Get(q)
 	resp, err := s.ReceiveSuccess(&result)
 	fmt.Println(resp.Status)
+
+	fmt.Println("starting download of", len(result), "files...")
+
 	if err != nil {
 		fmt.Println(err)
 	} else {
@@ -44,30 +67,31 @@ func main() {
 		}
 	}
 
+	close(filesToDownload)
+
 	wg.Wait()
+
+	fmt.Println("...operation completed")
 }
-func downloadArtifacts(circleToken string, artifacts chan ArtifactJson) {
+
+func downloadArtifacts(circleToken string, downloadFolder string, artifacts chan Artifact) {
 	for file := range artifacts {
-		t := time.Now()
+		go func() {
+			t := time.Now()
 
-		out, _ := os.Create(strings.Replace(file.PrettyPath, "$CIRCLE_ARTIFACTS", ".", 1))
-		defer out.Close()
+			out, _ := os.Create(strings.Replace(file.PrettyPath, "$CIRCLE_ARTIFACTS", downloadFolder, 1))
+			defer out.Close()
 
-		q := fmt.Sprintf("%s?circle-token=%s", file.Url, circleToken)
+			q := fmt.Sprintf("%s?circle-token=%s", file.Url, circleToken)
 
-		resp, _ := http.DefaultClient.Get(q)
-		defer resp.Body.Close()
+			resp, _ := http.DefaultClient.Get(q)
+			defer resp.Body.Close()
 
-		n, _ := io.Copy(out, resp.Body)
-		fmt.Println("file", file.PrettyPath, time.Since(t), n, "bytes")
+			n, _ := io.Copy(out, resp.Body)
+			fmt.Println("file", file.PrettyPath, time.Since(t), n, "bytes")
 
-		wg.Done()
+			wg.Done()
+		}()
 	}
 }
 
-type ArtifactJson struct {
-	Path       string `json:"path"`
-	PrettyPath string `json:"pretty_path"`
-	NodeIndex  string `json:"nodex_index"`
-	Url        string `json:"url"`
-}
